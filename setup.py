3097fd61de8018cc585515c232abcb2eb15222f8
Tim Hopper <tdhopper@gmail.com>
2024-01-16 18:21:32 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="setuptools-with-setuppy",
-    version="0.1",
+    version="0.2",
     description="This is a demo package",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
